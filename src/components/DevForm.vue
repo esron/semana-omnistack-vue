1a3fd8bb397b3f22009aeff4763e9670fcdb652d
Esron Dtamar <esron.dtamar@gmail.com>
2020-02-11 20:16:54 -0300

    cria componente DevForm

---
+++ src/components/DevForm.vue
@@ -0,0 +1,35 @@
+<template>
+  <form>
+    <div>
+      <label for="github_username">Usuário do Github</label>
+      <input
+        type="text"
+        id="github_username"
+        required
+      />
+    </div>
+
+    <div>
+      <label for="github_username">Tecnologias</label>
+      <input
+        type="text"
+        id="github_username"
+        required
+      />
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
