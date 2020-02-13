f21f7c012ecdf910f35e884b637d8cf887beaf85
Esron Dtamar <esron.dtamar@gmail.com>
2020-02-13 20:20:49 -0300

    fix inserção de text da prop do componente

--- src/components/DevItem.vue
+++ src/components/DevItem.vue
@@ -6,11 +6,11 @@
         :alt=dev.name
       />
       <div class="user-info">
-        <strong>{dev.name}</strong>
-        <span>{dev.techs.join(', ')}</span>
+        <strong>{{dev.name}}</strong>
+        <span>{{dev.techs.join(', ')}}</span>
       </div>
     </header>
-    <p>{dev.bio}</p>
+    <p>{{dev.bio}}</p>
     <a href="`http://github.com/${dev.github_username}`">Acessar perfil no Github</a>
   </li>
 </template>
