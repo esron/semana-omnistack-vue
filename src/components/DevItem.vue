2c5fa7251f69b3ac34b9946ff6e08e96bed1b5ff
Esron Dtamar <esron.dtamar@gmail.com>
2020-02-13 20:15:44 -0300

    cria o componete DevItem

---
+++ src/components/DevItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <li class="dev-item">
+    <header>
+      <img
+        :src=dev.avatar_url
+        :alt=dev.name
+      />
+      <div class="user-info">
+        <strong>{dev.name}</strong>
+        <span>{dev.techs.join(', ')}</span>
+      </div>
+    </header>
+    <p>{dev.bio}</p>
+    <a href="`http://github.com/${dev.github_username}`">Acessar perfil no Github</a>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    dev: () => {},
+  },
+  name: 'DevItem',
+}
+</script>
